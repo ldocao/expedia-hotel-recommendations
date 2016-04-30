df223ee76be615adb013f1cd6e5ffb940023b076
ldocao <ldocao@quantmetry.com>
2016-04-30 23:55:00 +0200

    refactor: make it more readable

--- app/feature_engineering/convert_dtypes.py
+++ app/feature_engineering/convert_dtypes.py
@@ -1,5 +1,19 @@
 def pseudo_numerics_to_string(data):
-    for i in [1,2,3,4,5,7,8,9,10,16,17,18,20,21,22,23] :
-        data[data.columns[i]] = data[data.columns[i]].apply(str)
+    column_to_convert = ['site_name',
+                         'posa_continent',
+                         'user_location_country',
+                         'user_location_region',
+                         'user_location_city',
+                         'user_id',
+                         'channel',
+                         'srch_destination_id',
+                         'srch_destination_type_id',
+                         'hotel_continent',
+                         'hotel_country',
+                         'hotel_market',
+                         'hotel_cluster']
+    
+    for col in column_to_convert :
+        data[col] = data[col].apply(str)
 
     return data
