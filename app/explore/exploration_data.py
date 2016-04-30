7731c2cb74f6793c5a7e83e908c2425781889906
Etienne Morey <morey@lerma101.(none)>
2016-04-30 12:21:31 +0200

    First data exploration

---
+++ app/explore/exploration_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data_expedia = pd.read_csv("../../../data_expedia/train_etienne.csv", nrows=100000)
+
+regions=data_expedia['user_location_region']
+
+sns.distplot(regions)
+plt.savefig('plot_regions.pdf')
+
