6f2a60c96d9882e62dfdddbf60159e0d81dc0c84
ldocao <ldocao@quantmetry.com>
2016-04-30 23:38:25 +0200

    fix: try/except is on datetime.datetime

--- app/feature_engineering/duration.py
+++ app/feature_engineering/duration.py
@@ -2,9 +2,9 @@ import datetime
 import numpy as np
 
 def between_date(begin_time_as_str, end_time_as_str):
-    begin_time = datetime.datetime.strptime(begin_time_as_str, "%Y-%m-%d")
-    end_time = datetime.datetime.strptime(end_time_as_str, "%Y-%m-%d")
     try:
+        begin_time = datetime.datetime.strptime(begin_time_as_str, "%Y-%m-%d")
+        end_time = datetime.datetime.strptime(end_time_as_str, "%Y-%m-%d")
         return end_time - begin_time
     except TypeError:
         return np.nan
@@ -12,9 +12,9 @@ def between_date(begin_time_as_str, end_time_as_str):
 
 
 def between_datetime(begin_time_as_str, end_time_as_str):
-    begin_time = datetime.datetime.strptime(begin_time_as_str, "%Y-%m-%d %H:%M:%S")
-    end_time = datetime.datetime.strptime(end_time_as_str, "%Y-%m-%d")
     try:
+        begin_time = datetime.datetime.strptime(begin_time_as_str, "%Y-%m-%d %H:%M:%S")
+        end_time = datetime.datetime.strptime(end_time_as_str, "%Y-%m-%d")
         return end_time - begin_time
     except TypeError:
         return np.nan
