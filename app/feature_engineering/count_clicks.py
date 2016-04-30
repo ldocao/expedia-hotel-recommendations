7b69b8e22222342d0349b60053611b4559b48da9
ldocao <ldocao@quantmetry.com>
2016-04-30 16:04:54 +0200

    refactor: modulate data dir

--- app/feature_engineering/count_clicks.py
+++ app/feature_engineering/count_clicks.py
@@ -3,11 +3,14 @@
 import pandas as pd
 import numpy as np
 
-data = "../../../data/train_etienne.csv"
+
+data_dir = "../../../data/" 
+source = data_dir + "train_etienne.csv"
+destination = data_dir + "user_click_counts.csv"
 df = pd.read_csv(data)
 
 
 sum_counts_per_user = df.groupby("user_id")["cnt"].sum()
 sum_counts_per_user = sum_counts_per_user.reset_index()
 sum_counts_per_user.columns = ["user_id", "total_number_of_clicks"]
-sum_counts_per_user.to_csv("../../../data/user_click_counts.csv", index=False, header=True)
+sum_counts_per_user.to_csv(destination, index=False, header=True)
