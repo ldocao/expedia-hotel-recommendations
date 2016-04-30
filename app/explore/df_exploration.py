c299888929a0ca656a5c04e015e8b0fe25ab6818
yaochloe <yaochloe@hotmail.com>
2016-04-30 13:06:45 +0200

    create hist of number of booking

--- app/explore/df_exploration.py
+++ app/explore/df_exploration.py
@@ -5,10 +5,17 @@ import matplotlib.pyplot as plt
 # Load Database
 df=pd.read_csv("../../../train_etienne.csv",nrows=10000)
 
+# Distribution of posa_continent
 plt.figure()
-
 sns.distplot(df["posa_continent"],kde=False)
 plt.yscale("log")
 plt.ylabel("Counts")
 plt.title("Distribution of posa_continent")
 plt.savefig("posa_continent_hist.pdf")
+
+# Distribution of is_booking 
+plt.figure()
+sns.distplot(df.groupby("user_id")["is_booking"].sum(),kde=False)
+plt.ylabel("number of users")
+plt.title("Distribution of number of booking")
+plt.savefig("is_booking_hist.pdf")
---
+++ sum.py
@@ -0,0 +1,7 @@
+def do_sum(max_number):
+	sum_of_integers = 0
+	for i in range(1,max_number+1):
+		sum_of_integers += i
+	return sum_of_integers
+
+
