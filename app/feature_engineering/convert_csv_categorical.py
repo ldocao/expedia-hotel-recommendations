b6782253a34a5e0ab27e835f50721b4b96b46a95
ldocao <ldocao@quantmetry.com>
2016-04-30 16:54:20 +0200

    fix: change dir

--- app/feature_engineering/convert_csv_categorical.py
+++ app/feature_engineering/convert_csv_categorical.py
@@ -1,2 +1,12 @@
-# -*- coding: utf-8 -*-
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data_expedia = pd.read_csv("../../../data/train_etienne.csv")#, nrows=10)
+
+
+for i in [1,2,3,4,5,7,8,9,10,16,17,18,20,21,22,23] :
+	data_expedia[data_expedia.columns[i]] = data_expedia[data_expedia.columns[i]].apply(str)
+
+data_expedia.to_csv('train_string_data.csv')
 
