0363e65d74851623c8af20ab11702ffc1b36783d
yaochloe <yaochloe@hotmail.com>
2016-05-01 15:51:54 +0200

    fit and prediction

--- app/predict/linear_regression.py
+++ app/predict/linear_regression.py
@@ -2,14 +2,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn import datasets, linear_model
+from sklearn.cross_validation import train_test_split
+from math import sqrt
 import ipdb
+import numpy as np
 
 
 # Load Database
-train_df=pd.read_csv("train_target.csv").dropna()
+df=pd.read_csv("train_target.csv").dropna()
 
 
-features=train_df[["orig_destination_distance", #variable to normalize
+features=df[["orig_destination_distance", #variable to normalize
 				"srch_adults_cnt",
 				"srch_children_cnt",
 				"srch_rm_cnt",
@@ -17,10 +20,21 @@ features=train_df[["orig_destination_distance", #variable to normalize
 				"stay_duration",
 				"is_mobile",
 				"is_package"]]
+features_norm = (features-features.mean())/features.std()
 
+target=df["appetence"]
 
-target=train_df["appetence"]
+features_train, features_test, target_train, target_test = train_test_split(features_norm, target, test_size=0.2, random_state=1)
+
 
 reg = linear_model.LinearRegression()
-reg.fit(features, target)
+reg.fit(features_train, target_train)
+
 print('Coefficients: \n', reg.coef_)  
+
+target_test_predicted = reg.predict(features_test)
+
+#sns.distplot(np.sqrt((target_test_predicted-target_test)**2),kde=False, bins=100)
+sns.distplot(((target_test_predicted-target_test)),kde=False, bins=100)
+#plt.yscale("log")
+plt.show()
