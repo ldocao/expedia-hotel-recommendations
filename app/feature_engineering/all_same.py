9c2c2987f14d776893fe5727c2ba5ec34e7e91f5
ldocao <ldocao@quantmetry.com>
2016-05-01 09:35:10 +0200

    feat: unique_pairs

--- app/feature_engineering/all_same.py
+++
@@ -1,2 +0,0 @@
-def all_same(list_of_strings):
-	return all(x == list_of_strings[0] for x in list_of_strings)
--- app/feature_engineering/group_by.py
+++ app/feature_engineering/group_by.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import datetime
 from feature_engineering.duration import between_date, between_datetime
+from feature_engineering.most_common import most_common
 
 featured_train = "../../../data/train_final.csv"
 non_unique_pairs = pd.read_csv(featured_train, nrows=1000)
@@ -9,7 +10,7 @@ non_unique_pairs = pd.read_csv(featured_train, nrows=1000)
 
 
 ## restore date time
-non_unique_pairs["date_time"] = non_unique_pairs["date_time"].apply(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S"))
+non_unique_pairs["date_time"] = non_unique_pairs["date_time"].apply(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").date())
 
 
 group = non_unique_pairs.groupby(["user_id", "hotel_cluster"])
@@ -26,4 +27,10 @@ unique_pairs = group.agg({'orig_destination_distance':'mean',
                           'stay_duration':'mean',
                           'time_to_travel':'mean'})
 
+unique_pairs["date_time"] = group["date_time"].agg(most_common)
+unique_pairs["srch_ci"] = group["srch_ci"].agg(most_common)
+unique_pairs["hotel_market"] = group["hotel_market"].agg(most_common)
+unique_pairs["srch_destination_id"] = group["srch_destination_id"].agg(most_common)
+unique_pairs["user_location_city"] = group["user_location_city"].agg(most_common)
+unique_pairs["user_location_region"] = group["user_location_region"].agg(most_common)
 
